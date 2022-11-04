69d3f172e7e87ad43c1315fdc82a062dd64aa231
Sabdiel <72673576+Sabdiel@users.noreply.github.com>
2022-11-03 18:31:32 -0600

    actualización de parametros de creacion

--- creation-script-2.sql
+++ creation-script-2.sql
@@ -10,12 +10,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema deadstrike
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `deadstrike` ;
-
--- -----------------------------------------------------
--- Schema deadstrike
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `deadstrike` DEFAULT CHARACTER SET utf8 ;
+CREATE DATABASE IF NOT EXISTS `deadstrike` DEFAULT CHARACTER SET utf8 ;
 USE `deadstrike` ;
 
 -- -----------------------------------------------------
